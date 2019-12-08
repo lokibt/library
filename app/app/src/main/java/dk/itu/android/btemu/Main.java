2d578bbf9a33b03e25fea471bcb74af2d83ecb01
Torben <torben.letorbi@gmail.com>
2019-12-08 02:58:24 +0100

    Clean up and refactor btemu app code

--- app/app/src/main/java/dk/itu/android/btemu/Main.java
+++ app/app/src/main/java/dk/itu/android/btemu/Main.java
@@ -9,13 +9,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-import android.Manifest;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -25,9 +23,6 @@ import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.AdapterView.OnItemClickListener;
-
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import dk.itu.android.bluetooth.BluetoothAdapter;
 import dk.itu.android.bluetooth.BluetoothDevice;
@@ -104,17 +99,16 @@ public class Main extends Activity {
 		}
 	};
 
-	SimpleAdapter adapter;
-	List<Map<String,Object>> list = new ArrayList<Map<String,Object>>();
-	ListView devices;
-	BluetoothAdapter bta;
+	private SimpleAdapter listAdapter;
+	private List<Map<String,Object>> list = new ArrayList<Map<String,Object>>();
+	private BluetoothAdapter bluetoothAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        bta = BluetoothAdapter.getDefaultAdapter();
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
 		IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
 		filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
@@ -124,16 +118,15 @@ public class Main extends Activity {
 		filter.addAction(BluetoothDevice.ACTION_FOUND);
 		registerReceiver(mReceiver, filter);
 
-		devices = (ListView)findViewById(R.id.Devices);
-		adapter = new SimpleAdapter(this, list, R.layout.row, new String[]{ITEM_KEY}, new int[]{R.id.list_value});
-		this.devices.setAdapter(adapter);
+		ListView deviceList = findViewById(R.id.Devices);
+		listAdapter = new SimpleAdapter(this, list, R.layout.row, new String[]{ITEM_KEY}, new int[]{R.id.list_value});
+		deviceList.setAdapter(listAdapter);
 
-		Button enableBtn = (Button)findViewById(R.id.Enable);
-		Button disableBtn = (Button)findViewById(R.id.Disable);
-		Button discoverableBtn = (Button)findViewById(R.id.Discoverable);
-		Button discoveryBtn = (Button)findViewById(R.id.Discovery);
-		Button serverBtn = (Button)findViewById(R.id.StartServer);
-		Button clientBtn = (Button)findViewById(R.id.StartClient);
+		Button enableBtn = findViewById(R.id.Enable);
+		Button disableBtn = findViewById(R.id.Disable);
+		Button discoverableBtn = findViewById(R.id.Discoverable);
+		Button discoveryBtn = findViewById(R.id.Discovery);
+		Button serverBtn = findViewById(R.id.StartServer);
 
 		enableBtn.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -148,7 +141,7 @@ public class Main extends Activity {
 			@Override
 			public void onClick(View v) {
 				Log.i(TAG, "Disabling emulator...");
-				bta.disable();
+				bluetoothAdapter.disable();
 			}
 		});
 
@@ -166,8 +159,8 @@ public class Main extends Activity {
 			public void onClick(View v) {
 				Log.i(TAG, "Starting discovery...");
 				list.clear();
-				adapter.notifyDataSetChanged();
-				bta.startDiscovery();
+				listAdapter.notifyDataSetChanged();
+				bluetoothAdapter.startDiscovery();
 			}
 		});
 
@@ -178,27 +171,82 @@ public class Main extends Activity {
 			}
 		});
 
-		clientBtn.setOnClickListener(new View.OnClickListener() {
+		deviceList.setOnItemClickListener(new OnItemClickListener() {
 			@Override
-			public void onClick(View v) {
-				startClient();
-			}
-		});
-
-		devices.setOnItemClickListener(new OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int position,
-					long arg3) {
-				Log.i("BTEMU_TEST", "selecting remote device at position: "+position);
-				other = (BluetoothDevice)list.get(position).get("DEVICE");
-				Log.i("BTEMU_TEST", "other device is: "+other);
+			public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+				BluetoothDevice other = (BluetoothDevice)list.get(position).get("DEVICE");
+				Log.d(TAG, "other device is: " + other);
+				startClient(other);
 			}
 		});
     }
 
+	private void addDevice(final BluetoothDevice device) {
+    	Log.d(TAG, "got device! " + device);
+    	Map<String,Object> item = new HashMap<String,Object>(){{
+    		put(ITEM_KEY, device.getName() + " - " + device.getAddress());
+    		put("DEVICE", device);
+    	}};
+    	list.add(item);
+    	listAdapter.notifyDataSetChanged();
+    }
+    
+    private void startServer() {
+    	new Thread(new Runnable() {
+    		@Override
+    		public void run() {
+    	    	try {
+    	    		Log.d(TAG, "Server accepting connection...");
+    				BluetoothServerSocket server = BluetoothAdapter
+    					.getDefaultAdapter()
+    					.listenUsingRfcommWithServiceRecord("dk.echo", UUID.fromString("419bbc68-c365-4c5e-8793-5ebff85b908c"));
+					Log.d(TAG, "Client creates client socket");
+    				BluetoothSocket client = server.accept();
+    				String line = new BufferedReader(new InputStreamReader(client.getInputStream())).readLine();
+    	    		Log.d(TAG, "Server sends echo for line: " +  line);
+    				client.getOutputStream().write( ("echoed: " + line).getBytes("UTF-8") );
+    	    		Log.d(TAG, "Server is closing client and server socket");
+    				client.close();
+    				server.close();
+    			} catch (Exception e) {
+    				Log.e(TAG, "Error in echo server", e);
+    			}
+    		}
+    	}).start();
+    }
+
+    private void startClient(final BluetoothDevice other) {
+    	new Thread(new Runnable() {
+    		@Override
+    		public void run() {
+    	    	try {
+    	    		Log.d(TAG, "Client creates socket");
+    				BluetoothSocket s = other.createRfcommSocketToServiceRecord(UUID.fromString("419bbc68-c365-4c5e-8793-5ebff85b908c"));
+    	    		Log.d(TAG, "Client connects to socket");
+    	    		s.connect();
+    	    		Log.d(TAG, "Client sends message");
+    				s.getOutputStream().write("Hello Bluetooth :)\r\n".getBytes("UTF-8"));
+    				s.getOutputStream().flush();
+    				Log.d(TAG, "Client reads response");
+    				final String reply = new BufferedReader(new InputStreamReader(s.getInputStream())).readLine();
+    	    		Log.d(TAG, "Client is closing socket");
+    				s.close();
+    				runOnUiThread(new Runnable(){
+    					@Override
+    					public void run() {
+    						((TextView)findViewById(R.id.EchoResponse)).setText(reply);
+    					}
+    				});
+    			} catch (IOException e) {
+    				Log.e(TAG, "Error in echo client", e);
+    			}
+    		}
+    	}).start();
+    }
+
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    	switch (requestCode) {
+		switch (requestCode) {
 			case REQUEST_DISCOVERABLE:
 				switch (resultCode) {
 					case RESULT_OK:
@@ -226,81 +274,5 @@ public class Main extends Activity {
 			default:
 				Log.e(TAG, "Unknown request code: " + requestCode);
 		}
-    }
-
-    BluetoothDevice other = null;
-
-    @SuppressWarnings("serial")
-	private void addDevice(final BluetoothDevice d) {
-//    	other = d;
-    	Log.i("TESTACTIVITY", "got device! " + d.getAddress());
-    	Map<String,Object> item = new HashMap<String,Object>(){{
-    		put(ITEM_KEY, d.getName() + " - " + d.getAddress());
-    		put("DEVICE", d);
-    	}};
-    	list.add(item);
-    	adapter.notifyDataSetChanged();
-    }
-    
-    private void startServer() {
-    	new Thread(new Runnable(){
-    		@Override
-    		public void run() {
-    	    	try {
-    	    		Log.i("BTTEST", "accepting bt connection...");
-    				BluetoothServerSocket server = BluetoothAdapter
-    					.getDefaultAdapter()
-    					.listenUsingRfcommWithServiceRecord("dk.echo", 
-    							UUID.fromString("419bbc68-c365-4c5e-8793-5ebff85b908c"));
-    				BluetoothSocket client = server.accept();
-    				String line = new BufferedReader(new InputStreamReader(client.getInputStream())).readLine();
-    	    		Log.i("BTTEST", "read: "+line);
-    				client.getOutputStream().write( ("echoed: "+line).getBytes("UTF-8") );
-    	    		Log.i("BTTEST", "replying");
-    				client.close();
-    				server.close();
-    			} catch (Exception e) {
-    				Log.e("BTTEST", "error in echo server", e);
-    			}
-    		}
-    	}).start();
-    }
-
-    String resp;
-    private void setResp(String r){resp=r;}
-    private void startClient() {
-    	Thread t =new Thread(new Runnable(){
-    		@Override
-    		public void run() {
-    	    	try {
-    	    		Log.i("BTTEST_CLIENT", "creating socket..");
-    				BluetoothSocket s = other.createRfcommSocketToServiceRecord(UUID.fromString("419bbc68-c365-4c5e-8793-5ebff85b908c"));
-    	    		Log.i("BTTEST_CLIENT", "socket created");
-    	    		s.connect();
-    	    		Log.i("BTTEST_CLIENT", "socket connected");
-    				s.getOutputStream().write("foo bar? baaaz\r\n".getBytes("UTF-8"));
-    				s.getOutputStream().flush();
-    				Log.i("BTTEST_CLIENT", "message wrote");
-    				String reply = new BufferedReader(new InputStreamReader(s.getInputStream())).readLine();
-    	    		Log.i("BTTEST_CLIENT", "reply: "+reply);
-    	    		setResp(reply);
-    				s.close();
-    				runOnUiThread(new Runnable(){
-    					@Override
-    					public void run() {
-    						((TextView)findViewById(R.id.EchoResponse)).setText(resp);
-    					}
-    				});
-//    				synchronized(lock) {
-//    					lock.notify();
-//    				}
-    			} catch (IOException e) {
-    				// TODO Auto-generated catch block
-    				e.printStackTrace();
-    				Log.e("BTTEST", "error in echo client", e);
-    			}
-    		}
-    	});
-    	t.start();
-    }
+	}
 }
