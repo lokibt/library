b676e3e407ae7541600a3b0f914574c4e34f95f4
Torben <git@letorbi.com>
2021-06-09 14:23:37 +0200

    Set correct scan mode when disabling Bluetooth

--- lib/src/main/java/com/lokibt/bluetooth/emulation/BluetoothAdapterEmulator.java
+++ lib/src/main/java/com/lokibt/bluetooth/emulation/BluetoothAdapterEmulator.java
@@ -101,6 +101,7 @@ public class BluetoothAdapterEmulator implements CommandCallback {
         BluetoothServerSocketEmulator.closeAllOpenSockets();
         BluetoothSocket.closeAllOpenSockets();
         setState(BluetoothAdapter.STATE_TURNING_OFF);
+        setScanMode(BluetoothAdapter.SCAN_MODE_NONE);
         setState(BluetoothAdapter.STATE_OFF);
         return true;
     }
